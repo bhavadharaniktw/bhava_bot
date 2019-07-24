9e4ec812783c0d0c157db8c233f93942a69eb592
Bhavadharani <bhavadharani@Bhavadharanis-MacBook-Pro.local>
2019-07-24 19:46:01 +0530

    success gin implemented

--- main.go
+++ main.go
@@ -1,41 +1,31 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	chat "google.golang.org/api/chat/v1"
 )
 
-func GetWeapons(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		fmt.Fprint(w, "not allowed")
-		return
-	}
-	var event chat.DeprecatedEvent
-
-	if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(err.Error()))
-		return
-	}
-	switch event.Type {
-	case "ADDED_TO_SPACE":
-		if event.Space.Type != "ROOM" {
-			break
-		}
-		fmt.Fprint(w, `{"text":"thanks for adding me."}`)
-	case "MESSAGE":
-		fmt.Fprintf(w, `{"text":"you said %s"}`, event.Message.Text)
-	}
-}
-
 func cool(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"text": "hello",
-	})
+
+	var json chat.DeprecatedEvent
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	switch json.Type {
+	case "ADDED_TO_SPACE":
+		c.JSON(200, gin.H{
+			"text": "thanks for adding me.",
+		})
+	case "MESSAGE":
+		c.JSON(200, gin.H{
+			"text": "hello " + json.User.DisplayName + ", you replied with " + json.Message.Text,
+		})
+	}
+
 }
 
 func main() {
