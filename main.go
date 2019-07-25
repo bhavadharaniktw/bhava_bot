8ed0e12526f59905390c86b00d2ea1febe299a86
Bhavadharani <bhavadharani@Bhavadharanis-MacBook-Pro.local>
2019-07-25 12:05:24 +0530

    logged body and header of a request

--- main.go
+++ main.go
@@ -1,11 +1,49 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	chat "google.golang.org/api/chat/v1"
 )
+
+func RequestLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		body_buff, _ := ioutil.ReadAll(c.Request.Body)
+		rdr1 := ioutil.NopCloser(bytes.NewBuffer(body_buff))
+		rdr2 := ioutil.NopCloser(bytes.NewBuffer(body_buff)) //We have to create a new Buffer, because rdr1 will be read.
+
+		fmt.Println(readBody(rdr1)) // Print request body
+
+		c.Request.Body = rdr2
+		fmt.Println("\nHeader:\n")
+		fmt.Println(c.Request.Header)
+		c.Next()
+	}
+}
+
+func readBody(reader io.Reader) string {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(reader)
+
+	s := buf.String()
+	return s
+}
+
+func cool_init(c *gin.Context) {
+
+	// Set example variable
+	c.Set("example", "12345")
+
+	// before request
+
+	c.Next()
+
+}
 
 func cool(c *gin.Context) {
 
@@ -22,14 +60,15 @@ func cool(c *gin.Context) {
 		})
 	case "MESSAGE":
 		c.JSON(200, gin.H{
-			"text": "hello " + json.User.DisplayName + ", you replied with " + json.Message.Text,
+			"text": "thanks for adding me." + c.MustGet("example").(string),
 		})
 	}
 
 }
 
 func main() {
-	r := gin.Default()
-	r.POST("/", cool)
+	r := gin.New()
+	r.Use(RequestLogger())
+	r.POST("/", cool_init, cool)
 	r.Run()
 }
